8bb6e2fac2a90e02e670c4e3d1a0a0d7b16f1261
estikornelia <estikornelia@gmail.com>
2015-11-22 16:01:49 +0100

    My Django Girls app, first commit

---
+++ python_intro.py
@@ -0,0 +1,25 @@
+#def hi():
+#	print('Hi there!')
+#	print('How are you?')
+#hi()
+#def hi(name):
+#	if name == 'Ola':
+#		print('Hi Ola')
+#	elif name == 'Sonja':
+#		print('Hi Sonja')
+#	else:
+#		print('Hi anonymus')
+#hi('Sonja')
+#def hi(name):
+#	print('Hi ' + name + '!')
+#hi("Rachel")
+#girls = ['Szimo', 'Anita', 'Hajni']
+#for name in girls:
+def hi(name):
+	print('Hi ' + name + '!')
+girls = ['Szimo', 'Anita', 'Hajni']
+for name in girls:
+	hi(name)
+	print('Next girl')
+for i in range(1, 6):
+	print(i)
